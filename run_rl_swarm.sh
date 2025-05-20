94e1753337caaa99d28ab5b38b8c571353f5a6d4
whalepiz <97138501+whalepiz@users.noreply.github.com>
2025-05-20 09:21:48 +0700

    Update run_rl_swarm.sh

--- run_rl_swarm.sh
+++ run_rl_swarm.sh
@@ -594,7 +594,7 @@ echo -e "\n${GREEN}${BOLD}[✓] Good luck in the swarm! Your training session is
 [ "$(uname)" = "Darwin" ] && sed -i '' -E 's/(startup_timeout: *float *= *)[0-9.]+/\1120/' $(python3 -c "import hivemind.p2p.p2p_daemon as m; print(m.__file__)") || sed -i -E 's/(startup_timeout: *float *= *)[0-9.]+/\1120/' $(python3 -c "import hivemind.p2p.p2p_daemon as m; print(m.__file__)")
 [ "$(uname)" = "Darwin" ] && sed -i '' -e '/bootstrap_timeout: Optional\[float\] = None/s//bootstrap_timeout: float = 120/' $(python3 -c 'import hivemind.dht.node as m; print(m.__file__)') || sed -i -e '/bootstrap_timeout: Optional\[float\] = None/s//bootstrap_timeout: float = 120/' $(python3 -c 'import hivemind.dht.node as m; print(m.__file__)')
 if [ -n "$ORG_ID" ]; then
-    numactl --cpunodebind=0,1 --interleave=all python -m hivemind_exp.gsm8k.train_single_gpu \
+    numactl --cpunodebind -N 0,1 --interleave=all python -m hivemind_exp.gsm8k.train_single_gpu \
         --hf_token "$HUGGINGFACE_ACCESS_TOKEN" \
         --identity_path "$IDENTITY_PATH" \
         --modal_org_id "$ORG_ID" \
@@ -602,7 +602,7 @@ if [ -n "$ORG_ID" ]; then
         --config "$CONFIG_PATH" \
         --game "$GAME"
 else
-    numactl --cpunodebind=0,1 --interleave=all python -m hivemind_exp.gsm8k.train_single_gpu \
+    numactl --cpunodebind -N 0,1 --interleave=all python -m hivemind_exp.gsm8k.train_single_gpu \
         --hf_token "$HUGGINGFACE_ACCESS_TOKEN" \
         --identity_path "$IDENTITY_PATH" \
         --public_maddr "$PUB_MULTI_ADDRS" \
